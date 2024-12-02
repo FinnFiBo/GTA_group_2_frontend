c8b019d28aede12c11ffcf81d89e2d1fdd1e7db1
FinnFiBo <finn.bogatu@gmail.com>
2024-12-02 12:39:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -429,7 +429,14 @@ function get_ri(latlng) {
     let lat = latlng.lat;
     let lng = latlng.lng;
     // Hier RI-Wert anpassen oder berechnen
-    fetch(`${app_url}/calculate_ri?lat=${lat}&lng=${lng}`,{ method: "GET" , headers: { 'Content-Type': 'application/json' } })
+    fetch(`${app_url}/calculate_ri?lat=${lat}&lng=${lng}`, { 
+        method: "GET", 
+        headers: { 
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        mode: 'cors'
+    })
         .then(response => {response.json();console.log(response);})
         .then(data => {
             if (data.error) {

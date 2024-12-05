0076a771091d1174d9b31ab241c8fcec49cf4a68
FinnFiBo <finn.bogatu@gmail.com>
2024-12-05 14:50:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -488,5 +488,12 @@ function register() {
 }
 
 function hashPassword(password) {
-    return CryptoJS.SHA256(password).toString();
+    let hash = 0;
+    if (password.length === 0) return hash.toString();
+    for (let i = 0; i < password.length; i++) {
+        const char = password.charCodeAt(i);
+        hash = (hash << 5) - hash + char;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash.toString();
 }
